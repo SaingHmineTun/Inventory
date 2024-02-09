d0d9b214879e919639acf7b05264b3acc7971cab
Sai Saing Hmine Tun <saisainghminetun@gmail.com>
2024-02-09 16:59:32 +0630

    Initial commit

---
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -0,0 +1,28 @@
+package com.example.inventory.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ItemDao {
+    // Inserting items with the same primary key will be ignored
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertItem(item: Item)
+
+    @Update
+    suspend fun updateItem(item: Item)
+
+    @Delete
+    suspend fun deleteItem(item: Item)
+
+    @Query("SELECT * FROM items ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Item>>
+
+    @Query("SELECT * FROM items WHERE id=:id")
+    fun getItemById(id: Int): Flow<Item>
+}
